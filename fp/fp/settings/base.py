3c22cf61325df80b0d9dcd8002314ab6d77845f9
Stephan Groß <stephan@minddust.com>
2013-11-05 14:26:20 +0100

    Fix project roots

--- fp/fp/settings/base.py
+++ fp/fp/settings/base.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 from __future__ import unicode_literals
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
