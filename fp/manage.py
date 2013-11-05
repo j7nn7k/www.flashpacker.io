7d5be56fbd8d74268547f9f9faf59ab80b49b6ec
Stephan Groß <stephan@minddust.com>
2013-11-05 14:23:23 +0100

    Switch quotes due to styleguide

--- fp/manage.py
+++ fp/manage.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fp.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fp.settings')
 
     from django.core.management import execute_from_command_line
 
